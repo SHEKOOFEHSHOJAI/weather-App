0b0e0c35f4af2c5bb4f3d495363ee44cd95aa5f8
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-22 22:29:07 -0700

    shekoofeh-shojai

--- weather-App/scripts/app.js
+++ weather-App/scripts/app.js
@@ -1,12 +1,32 @@
+//get wather
+const getweather=async(id)=>{
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data;
+}
+
+
+//get information
+
 //get https://developer.accuweather.com/
-const key = 'n0JY9ASOb13UqhjxArTx6faicvRsnBnt';
+const key = "	GVt5hQJozPYX9YH4BSAaWx1gFAzC4XzY";
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apiKey=${Key}&q=${city}`;
-  const response=await fetch(base+query)
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
   const data=await response.json();
-  console.log(data);
+  return data[0]
 };
       
-getCity("manchester")
+getCity("landan")
+.then(data=>{
 
+  console.log(data);
+ return getweather(data.Key);
+}).then(data=>{
+  
+  console.log(data);
+})
+.catch(eroor=>console.error(eroor))
